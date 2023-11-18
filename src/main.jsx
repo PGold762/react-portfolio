7f7329d99b1db1a0f11664b0c070b8ac6e3e3bba
PGold <pglz762@gmail.com>
2023-11-18 12:03:48 -0600

    Initial files created and base code started

---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import ReactDOM from 'react-dom/client';
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import AboutPage from './pages/AboutPage';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
